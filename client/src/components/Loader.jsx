6ee343e3b579699c334401cb32f92eb64468b9c9
surya <v.suryaprashanth@gmail.com>
2024-11-18 18:56:38 +0530

    Final commit

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,20 +1,20 @@
-import {Grid} from 'react-loader-spinner'
+import { Grid } from "react-loader-spinner";
 
-const Loader = ()=>{
-    return (
-        <div className='z-10'>
-            <Grid
-                visible={true}
-                height="110"
-                width="110"
-                color="#00a200"
-                ariaLabel="grid-loading"
-                radius="12.5"
-                wrapperStyle={{}}
-                wrapperClass="grid-wrapper"
-            />
-        </div>
-    )
-}
+const Loader = () => {
+  return (
+    <div className="z-10">
+      <Grid
+        visible={true}
+        height="110"
+        width="110"
+        color="#0087bd"
+        ariaLabel="grid-loading"
+        radius="12.5"
+        wrapperStyle={{}}
+        wrapperClass="grid-wrapper"
+      />
+    </div>
+  );
+};
 
 export default Loader;
--- client/src/components/LoaderFull.jsx
+++ client/src/components/LoaderFull.jsx
@@ -1,20 +1,20 @@
-import {Grid} from 'react-loader-spinner'
+import { Grid } from "react-loader-spinner";
 
-const LoaderFull = ()=>{
-    return (
-        <div className='absolute top-0 min-h-screen min-w-full flex items-center justify-center bg-black/20'>
-            <Grid
-                visible={true}
-                height="110"
-                width="110"
-                color="#00a200"
-                ariaLabel="grid-loading"
-                radius="12.5"
-                wrapperStyle={{}}
-                wrapperClass="grid-wrapper"
-            />
-        </div>
-    )
-}
+const LoaderFull = () => {
+  return (
+    <div className="absolute top-0 min-h-screen min-w-full flex items-center justify-center bg-black/20">
+      <Grid
+        visible={true}
+        height="110"
+        width="110"
+        color="#0087bd"
+        ariaLabel="grid-loading"
+        radius="12.5"
+        wrapperStyle={{}}
+        wrapperClass="grid-wrapper"
+      />
+    </div>
+  );
+};
 
 export default LoaderFull;
